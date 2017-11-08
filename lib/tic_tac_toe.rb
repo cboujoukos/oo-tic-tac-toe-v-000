8731d76d1b056d0bf1148af3a21a65f06e8cd2ad
Christopher Boujoukos <cboujoukos@gmail.com>
2017-11-08 16:44:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,8 +75,10 @@ def won?
     position_2 = @board[win_index_2]
     position_3 = @board[win_index_3]
 
-    (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
     (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return win_combo
+    end
 end
 
 end
